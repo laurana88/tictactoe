e69e5ca1b2d277839ed4452459ec82caacba53c1
Laura Smith <laura.ann.smith88@gmail.com>
2021-06-24 11:53:22 -0400

    imlemented hooks and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# Getting Started with Create React App
+# Tic Tac Toe Game
+
+Created this game from the tutorial found on on [React JS](https://reactjs.org/tutorial/tutorial.html).
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Improvements
+
+### Implemented Hooks
+
+After reading about the new hooks available (specifically setState), I updated the code to use hooks.
+
+## Future Improvements
+
+These are based on the ideas listed in the tutorial.
+
+1. Display the location for each move in the format (col, row) in the move history list.
+2. Bold the currently selected item in the move list.
+3. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+4. Add a toggle button that lets you sort the moves in either ascending or descending order.
+5. When someone wins, highlight the three squares that caused the win.
+6. When no one wins, display a message about the result being a draw.
 
 ## Available Scripts
 
--- src/index.js
+++ src/index.js
@@ -13,41 +13,17 @@ const Square = (props) => {
   );
 }
 
-function Board() {
-
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xisnext, setXisnext] = useState(true);
-
-  const handleClick = (i) => {
-    const newSquares = squares.slice();
-
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    
-    newSquares[i] = xisnext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXisnext(!xisnext);
-  }
+const Board = (props) => {
 
   const renderSquare = (i) => {
     return <Square 
-      value={squares[i]}
-      onClick={() => handleClick(i)} 
+      value={props.squares[i]}
+      onClick={() => props.onClick(i)} 
     />;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xisnext ? 'X' : 'O');
   }
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -67,41 +43,91 @@ function Board() {
   );
 }
 
-function Game() {
+const Game = () => {
+
+  const [xisnext, setXisnext] = useState(true);
+  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const newHistory = history.slice(0, stepNumber + 1);
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const handleClick = (i) => {
+
+    const squares = current.squares.slice();
+    console.log("squares", squares);
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
+    squares[i] = xisnext ? 'X' : 'O';  
+    setXisnext(!xisnext);
+    setStepNumber(newHistory.length);
+    setHistory(newHistory.concat([{squares: squares}]));
+  }
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXisnext((step % 2) === 0);
+  }
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xisnext ? 'X' : 'O');
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board 
+          squares={current.squares}
+          onClick={(i) => handleClick(i)}
+        />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 // ========================================
 
 ReactDOM.render(<Game />, 
   document.getElementById('root'));
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+}
